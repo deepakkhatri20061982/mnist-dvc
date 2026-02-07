52805bcf8062b126083065c4726169da838d03c6
Deepak Khatri <deepakkhatri20061982@gmail.com>
2026-02-07 14:25:20 +0530

    Integration with MLFlow

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,35 +3,51 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import yaml
+import mlflow
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="pkg_resources")
+
+with open("experiments/experiment.yaml") as f:
+    exp_cfg = yaml.safe_load(f)
+experiment_name = exp_cfg["experiment_name"]
+mlflow.set_tracking_uri("http://host.docker.internal:5000")
+mlflow.set_experiment(experiment_name)
 
 with open("params.yaml") as f:
     params = yaml.safe_load(f)
 
 transform = [transforms.ToTensor()]
-if params["preprocess"]["normalize"]:
+normalize = params["preprocess"]["normalize"]
+if normalize:
     transform.append(transforms.Normalize((0.1307,), (0.3081,)))
 
 transform = transforms.Compose(transform)
 
-dataset = datasets.MNIST(
-    root="data/raw",
-    train=True,
-    download=True,
-    transform=transform
-)
+with mlflow.start_run(run_name="preprocess"):
+    mlflow.log_param("normalize", normalize)
+    dataset = datasets.MNIST(
+        root="data/raw",
+        train=True,
+        download=True,
+        transform=transform
+    )
 
-loader = DataLoader(dataset, batch_size=1000)
+    loader = DataLoader(dataset, batch_size=1000)
 
-os.makedirs("data/processed", exist_ok=True)
+    os.makedirs("data/processed", exist_ok=True)
 
-images, labels = [], []
+    images, labels = [], []
 
-for x, y in loader:
-    images.append(x)
-    labels.append(y)
+    for x, y in loader:
+        images.append(x)
+        labels.append(y)
 
-images = torch.cat(images)
-labels = torch.cat(labels)
+    images = torch.cat(images)
+    labels = torch.cat(labels)
 
-torch.save(images, "data/processed/images.pt")
-torch.save(labels, "data/processed/labels.pt")
+    os.makedirs("data/processed", exist_ok=True)
+    torch.save(images, "data/processed/images.pt")
+    torch.save(labels, "data/processed/labels.pt")
+    mlflow.log_metric("num_samples", len(images))
+    mlflow.log_artifact("data/processed/images.pt")
+
--- src/train.py
+++ src/train.py
@@ -4,31 +4,50 @@ import torch.optim as optim
 import json
 import yaml
 from model import LogisticRegression
+import mlflow
+import mlflow.pytorch
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="pkg_resources")
+
+with open("experiments/experiment.yaml") as f:
+    exp_cfg = yaml.safe_load(f)
+experiment_name = exp_cfg["experiment_name"]
+
+mlflow.set_tracking_uri("http://host.docker.internal:5000")
+mlflow.set_experiment(experiment_name)
 
 with open("params.yaml") as f:
     params = yaml.safe_load(f)
 
+lr = params["train"]["lr"]
+epochs = params["train"]["epochs"]
 images = torch.load("data/processed/images.pt")
 labels = torch.load("data/processed/labels.pt")
-
 images = images.view(images.size(0), -1)
 
 model = LogisticRegression()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=params["train"]["lr"])
+optimizer = optim.SGD(model.parameters(), lr=lr)
 
-for epoch in range(params["train"]["epochs"]):
-    outputs = model(images)
-    loss = criterion(outputs, labels)
+with mlflow.start_run(run_name="training"):
+    mlflow.log_param("lr", lr)
+    mlflow.log_param("epochs", epochs)
+    for epoch in range(epochs):
+        outputs = model(images)
+        loss = criterion(outputs, labels)
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-_, preds = torch.max(outputs, 1)
-accuracy = (preds == labels).float().mean().item()
+    _, preds = torch.max(outputs, 1)
+    accuracy = (preds == labels).float().mean().item()
 
-torch.save(model.state_dict(), "model.pt")
+    torch.save(model.state_dict(), "model.pt")
+    mlflow.log_metric("loss", loss.item())
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.pytorch.log_model(model, artifact_path="model")
 
-with open("metrics.json", "w") as f:
-    json.dump({"accuracy": accuracy, "loss": loss.item()}, f)
+    with open("metrics.json", "w") as f:
+        json.dump({"accuracy": accuracy, "loss": loss.item()}, f)
